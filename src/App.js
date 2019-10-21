198d7e298e3880d54530ef406f3632d0814911c9
yurilimalemes <yurilimalemes@gmail.com>
2019-10-21 02:06:07 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,162 @@
+import React, { Component } from "react";
+import { TextInput, Button, Card, Col, Row } from "react-materialize";
+import "./App.css";
+
+const female = {
+  duration: 1,
+  background:
+    "linear-gradient(to bottom right, rgb(205, 37, 207), rgb(256, 13, 175))",
+  border: "solid 2px #2B2D41",
+  color: "white"
+};
+
+const male = {
+  duration: 1,
+  background:
+    "linear-gradient(to bottom right, rgb(65, 77, 247), rgb(56, 103, 255))",
+  border: "solid 2px #2B2D41",
+  color: "white"
+};
+
+class App extends Component {
+  state = {
+    female: false,
+    male: false,
+    feet: 0,
+    weight: 0,
+    BMI: 0
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  femaleButton = e => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      female: !prevState.female,
+      male: false
+    }));
+  };
+
+  maleButton = e => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      male: !prevState.male,
+      female: false
+    }));
+  };
+
+  handleClose = () => {
+    this.setState({ show: false });
+  };
+
+  handleShow = () => {
+    this.setState({ show: true });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const state = this.state;
+    const weightCalculation = state.weight;
+    const heightCalc = state.feet;
+    const squarerootOfHeight = heightCalc ** 2;
+    const YourBMI = weightCalculation / squarerootOfHeight;
+
+    this.setState({
+      BMI: YourBMI.toFixed(2)
+    });
+  };
+
+  render() {
+    let weightCalc;
+    let weightText;
+    const values = this.state;
+
+    if (values.BMI > 0) {
+      weightCalc = <div>{values.BMI}</div>;
+    } else {
+      weightCalc = <div />;
+    }
+
+    if (values.BMI < 18.5 && values.BMI > 0) {
+      weightText = <p className="bmi-text underweight">UnderWeight</p>;
+    } else if (values.BMI > 18.5 && values.BMI < 24.99) {
+      weightText = <p className="bmi-text normal">Normal</p>;
+    } else if (values.BMI > 25 && values.BMI < 29.9) {
+      weightText = <p className="bmi-text overweight">Overweight</p>;
+    } else if (values.BMI > 30) {
+      weightText = <p className="bmi-text obese">Obese</p>;
+    }
+
+    return (
+      <div className="container-App">
+        <div className="App">
+          <h1 className="title-of-app">Indice Massa Muscular</h1>
+
+          <Col m={12} s={12}>
+            <form autoComplete="off" onSubmit={this.handleSubmit}>
+              <Card>
+                <div className="sex-orientation">
+                  {values.female === true ? (
+                    <div
+                      onClick={this.femaleButton}
+                      style={female}
+                      className="sex female"
+                    >
+                      <i className="sex female" />
+                    </div>
+                  ) : (
+                    <div onClick={this.femaleButton} className="sex female">
+                      <i className="fas fa-female" />
+                    </div>
+                  )}
+
+                  {values.male === true ? (
+                    <div
+                      onClick={this.maleButton}
+                      style={male}
+                      className="sex male"
+                    >
+                      <i className="sex male" />
+                    </div>
+                  ) : (
+                    <div onClick={this.maleButton} className="sex male">
+                      <i className="fas fa-male" />
+                    </div>
+                  )}
+                </div>
+                <TextInput
+                  className="input"
+                  placeholder="Peso: "
+                  name="weight"
+                  onChange={this.handleChange}
+                  required
+                />
+                <Row>
+                  <TextInput
+                    className="input"
+                    placeholder="Altura: "
+                    name="feet"
+                    onChange={this.handleChange}
+                    required
+                  />
+                </Row>
+                <div />
+                <Button type="submit">Calcular IMC</Button>
+                <div className="weight-container">
+                  <h3 className="weightCalc">{weightCalc}</h3>
+                  {weightText}
+                </div>
+              </Card>
+            </form>
+          </Col>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
